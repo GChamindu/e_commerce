1db5c65757a7b74d55b293949d7fc602a9e78da6
Gavishka <gavishka.rightmo@gmail.com>
2025-11-24 02:36:28 +0530

    do01 : imple ent  home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -56,7 +56,7 @@ const page = () => {
       <FlashSalesOne />
 
       {/* ProductListOne */}
-      <ProductListOne />
+      {/* <ProductListOne /> */}
 
       {/* OfferOne */}
       <OfferOne />
